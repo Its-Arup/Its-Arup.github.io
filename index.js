cc64efd5dc2d115140d5a06092c47dc54ec72578
Its-Arup <aruppaul1650@gmail.com>
2023-10-27 00:12:26 +0530

    About section done

--- index.js
+++ index.js
@@ -12,3 +12,40 @@ document.querySelectorAll(".nav-links").forEach((e)=>{
         nav_menu.classList.toggle("active")
     })
 })
+
+function resumeDownload(){
+    window.open(
+       "https://drive.google.com/file/d/1y4cLjeWSNTa4THsMxYXMW3dYPmdrS3Ll/view?usp=sharing"
+      );
+}
+
+
+let githubBtn = document.querySelector("#contact-github");
+
+githubBtn.addEventListener('click',function(){
+    
+    window.open(
+        "https://github.com/Its-Arup",
+        "_blank"
+      );
+
+})
+
+let linkedinBtn = document.querySelector(".fa-linkedin");
+
+linkedinBtn.addEventListener('click',function(){
+    
+    window.open(
+        "https://www.linkedin.com/in/arup-paul13",
+        "_blank"
+      );
+
+})
+
+let contactBtn = document.querySelector("#contact-btn");
+
+contactBtn.addEventListener('click',function(){
+    
+    window.location.href = '#contact';
+
+})
