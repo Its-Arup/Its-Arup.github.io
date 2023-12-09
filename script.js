4ba0e2568d3fc57187118de87075a8d5b67031ce
unknown <arupaul1650@gmail.com>
2023-12-09 22:14:22 +0530

    resume link updated

--- script.js
+++ script.js
@@ -59,13 +59,13 @@ var TxtType = function (el, toRotate, period) {
 
   // resume ------------- function resumeTab1() 
 function resumeTab1() {
-  const pdfUrl = "https://drive.google.com/file/d/1tLECbO5fV9EnqdC3e9TFEq4j3f6o78ow/view?usp=sharing";
+  const pdfUrl = "https://drive.google.com/file/d/1aWEZQFHJuUNS6-V3dUESirw-pudkvBqn/view?usp=sharing";
     window.open(pdfUrl, '_blank');
 }
 
 document.querySelector("#resume-link-2").addEventListener("click", () => {
   window.open(
-    "https://drive.google.com/file/d/1tLECbO5fV9EnqdC3e9TFEq4j3f6o78ow/view?usp=sharing",
+    "https://drive.google.com/file/d/1aWEZQFHJuUNS6-V3dUESirw-pudkvBqn/view?usp=sharing",
     "_blank"
   );
 });
